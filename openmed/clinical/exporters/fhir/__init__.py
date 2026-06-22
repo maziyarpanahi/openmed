2b9b870ab2362b3fb30d455d708f0b3485fd663b
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2026-06-22 09:11:06 +0200

    test: pin deterministic FHIR fullUrl contract

--- openmed/clinical/exporters/fhir/__init__.py
+++ openmed/clinical/exporters/fhir/__init__.py
@@ -3,5 +3,6 @@
 from __future__ import annotations
 
 from .bundle import to_bundle
+from .references import deterministic_fullurl
 
-__all__ = ["to_bundle"]
+__all__ = ["deterministic_fullurl", "to_bundle"]
