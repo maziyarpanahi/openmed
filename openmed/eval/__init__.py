cfc6e99ca73feca121274a9b4d5cc6a929e09242
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2026-06-24 21:16:21 +0200

    feat: add eval error analysis report

--- openmed/eval/__init__.py
+++ openmed/eval/__init__.py
@@ -26,6 +26,11 @@ from openmed.eval.calibrate import (
     load_calibration_thresholds,
     write_calibration_artifacts,
 )
+from openmed.eval.error_analysis import (
+    ErrorAnalysisReport,
+    ErrorSpanExample,
+    error_report,
+)
 from openmed.eval.harness import (
     BenchmarkFixture,
     FixtureResult,
@@ -75,6 +80,8 @@ __all__ = [
     "CalibrationThresholdSet",
     "DEVICE_TIERS",
     "EvalSpan",
+    "ErrorAnalysisReport",
+    "ErrorSpanExample",
     "FixtureResult",
     "GateCheck",
     "GateReport",
@@ -103,6 +110,7 @@ __all__ = [
     "compute_surrogate_consistency",
     "default_suite_calibration_samples",
     "evaluate_quant_recall_delta",
+    "error_report",
     "fit_calibration_thresholds",
     "generate_reid_leaderboard",
     "load_calibration_samples",
