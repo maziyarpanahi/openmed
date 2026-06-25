a6f981a2f8c21247c001cbf375ba9f7f14ef3df5
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2026-06-25 22:11:18 +0200

    feat: add CodeableConcept consistency check

--- openmed/clinical/exporters/__init__.py
+++ openmed/clinical/exporters/__init__.py
@@ -1,1 +1,13 @@
 """Exporters that turn clinical resources into interchange formats (FHIR/OMOP)."""
+
+from .codeable_concept_check import (
+    CodeableConceptFinding,
+    CodeableConceptFindingCode,
+    check_codeable_concept,
+)
+
+__all__ = [
+    "CodeableConceptFinding",
+    "CodeableConceptFindingCode",
+    "check_codeable_concept",
+]
