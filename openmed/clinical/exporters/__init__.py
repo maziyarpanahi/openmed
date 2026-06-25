15f4bd1ac24b0754d909d1fd478582370d050248
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2026-06-25 22:04:12 +0200

    Add code provenance stamping helper

--- openmed/clinical/exporters/__init__.py
+++ openmed/clinical/exporters/__init__.py
@@ -1,1 +1,13 @@
 """Exporters that turn clinical resources into interchange formats (FHIR/OMOP)."""
+
+from __future__ import annotations
+
+from .code_provenance import (
+    CODE_SYSTEM_VERSION_SOURCE_EXTENSION_URL,
+    stamp_coding_provenance,
+)
+
+__all__ = [
+    "CODE_SYSTEM_VERSION_SOURCE_EXTENSION_URL",
+    "stamp_coding_provenance",
+]
