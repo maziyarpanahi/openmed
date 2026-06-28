f2d5d6fe07c3e5ea1412e16036fbe04c170c4e65
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2026-06-28 06:26:19 +0200

    feat: add risk dashboard renderer

--- openmed/risk/__init__.py
+++ openmed/risk/__init__.py
@@ -4,7 +4,13 @@ Intended contents include quasi-identifier detection, uniqueness/k-anonymity
 measurement, and adversarial re-identification analysis.
 """
 
+from .dashboard import render_risk_dashboard, write_risk_dashboard
 from .kanon import kanon_report
 from .reid import risk_report
 
-__all__ = ["risk_report", "kanon_report"]
+__all__ = [
+    "risk_report",
+    "kanon_report",
+    "render_risk_dashboard",
+    "write_risk_dashboard",
+]
