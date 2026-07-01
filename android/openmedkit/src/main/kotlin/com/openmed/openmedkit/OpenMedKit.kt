2fde3556d84782a48513333a8669f815a2cc6abd
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2026-07-01 16:02:00 +0200

    Scaffold Android OpenMedKit module (#1114)

---
+++ android/openmedkit/src/main/kotlin/com/openmed/openmedkit/OpenMedKit.kt
@@ -0,0 +1,11 @@
+package com.openmed.openmedkit
+
+/**
+ * Public entry point for the Android OpenMedKit library scaffold.
+ */
+object OpenMedKit {
+    /**
+     * Placeholder version until Android artifacts are released.
+     */
+    const val VERSION = "0.0.0-dev"
+}
