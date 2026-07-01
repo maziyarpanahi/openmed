c826b00de2035657499516e1eaafef4f7f394e75
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2026-07-01 16:43:27 +0200

    Add Swift-Kotlin API parity checks

--- android/openmedkit/build.gradle.kts
+++ android/openmedkit/build.gradle.kts
@@ -23,12 +23,19 @@ android {
     testOptions {
         unitTests.isIncludeAndroidResources = true
     }
+
+    sourceSets {
+        getByName("test") {
+            resources.srcDir(rootProject.file("../fixtures/parity"))
+        }
+    }
 }
 
 dependencies {
     implementation("com.microsoft.onnxruntime:onnxruntime-android:1.20.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
 
+    testImplementation("org.json:json:20240303")
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.robolectric:robolectric:4.11.1")
 }
