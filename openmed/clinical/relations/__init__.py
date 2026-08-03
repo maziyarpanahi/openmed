7c87486ecd0e265eb1e85a51b7dafbb47613e6ae
Maziyar Panahi <5762953+maziyarpanahi@users.noreply.github.com>
2026-08-04 01:18:48 +0200

    feat: unify clinical relation extraction facade

--- openmed/clinical/relations/__init__.py
+++ openmed/clinical/relations/__init__.py
@@ -56,6 +56,7 @@ from .medication_links import (
     MedicationRelationScorer,
     MedicationStatementRecord,
     extract_medication_relations,
+    extract_relations,
     link_medication_attributes,
     reconstruct_medication_statements,
 )
@@ -68,7 +69,6 @@ from .multilingual import (
     RELATION_TYPE_REGISTRY,
     MultilingualRelation,
     available_multilingual_relation_languages,
-    extract_relations,
     map_relation_type,
     multilingual_relation_rules,
     relation_type_mapping,
