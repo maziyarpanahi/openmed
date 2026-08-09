2511c4e874bf61da5c26955009e7bda80bd967c8
Maziyar Panahi <5762953+maziyarpanahi@users.noreply.github.com>
2026-08-09 12:35:26 +0200

    feat: add clinical entity extraction helper

--- openmed/__init__.py
+++ openmed/__init__.py
@@ -31,6 +31,7 @@ if TYPE_CHECKING:
     )
     from .core.results import AnalyzeResult
     from .processing import BatchProcessor
+    from .processing.advanced_ner import extract_clinical_entities
     from .processing.sentences import SentenceSpan
 
 _LAZY_IMPORTS = {
@@ -165,6 +166,7 @@ _LAZY_IMPORTS = {
     "redact_dataset": ".processing",
     "sentence_utils": ".processing",
     "AdvancedNERProcessor": ".processing.advanced_ner",
+    "extract_clinical_entities": ".processing.advanced_ner",
     "StreamingReplayResult": ".processing.advanced_ner",
     "StreamingTokenClassifier": ".processing.advanced_ner",
     "create_advanced_processor": ".processing.advanced_ner",
@@ -856,6 +858,7 @@ __all__ = [
     "process_batch",
     "redact_dataset",
     "AdvancedNERProcessor",
+    "extract_clinical_entities",
     "StreamingReplayResult",
     "StreamingTokenClassifier",
     "create_advanced_processor",
